e385f75041a93ef48e2ee6d57468c534fecde9c7
Vignesh Shanmugam <vignesh.shanmugam22@gmail.com>
2016-09-19 16:33:32 +0200

    fix source map options

--- index.js
+++ index.js
@@ -4,26 +4,28 @@ const optimizeJs = require('optimize-js');
 const { RawSource } = require("webpack-sources");
 
 module.exports = class OptimizeJsPlugin {
-    constructor({ options = {} }) {
+    constructor(options = {}) {
         this.options = options;
     }
 
     apply(compiler) {
         const jsregex = /\.js($|\?)/i;
-         const useSourceMap = !!this.options.sourceMap;
 
         compiler.plugin('emit', (compilation, callback) => {
             compilation.chunks.forEach((chunk) => {
                 const files = [];
 
                 chunk.files.forEach(file => files.push(file));
+
                 files
                     .filter(file => jsregex.test(file))
                     .forEach(file => {
                         try {
                             const asset = compilation.assets[file];
                             const input = asset.source();
-                            const result = optimizeJs(input, useSourceMap);
+                            const result = optimizeJs(input, {
+                                sourceMap: !!this.options.sourceMap
+                            });
                             compilation.assets[file] = new RawSource(result);
                         } catch(e) {
                             compilation.errors.push(e);
